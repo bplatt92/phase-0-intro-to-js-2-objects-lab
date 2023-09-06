53eec08282db358061912c3540557d5d61a59ceb
Brent Platt <bplatt92@live.com>
2023-09-06 19:08:09 -0400

    Objects lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Brent", 
+    streetAddress : "123 Main St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    delete["name"];
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
